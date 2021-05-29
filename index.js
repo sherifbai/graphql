0ff511c659f01ac37e1c9f41c497517b508740b1
Sherif <godness980@gmail.com>
2021-05-29 21:26:55 +0500

    configured index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+
+app.use(function (error, req, res, next) {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+})
+
+
+const url = "mongodb+srv://Sherif:5P86BeLIEsytx1Xg@cluster0.qwr9u.mongodb.net/graphql?retryWrites=true&w=majority"
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(result =>{
+    app.listen(8080)
+    console.log('Connected!!!')
+})
